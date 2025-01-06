1fa76f621271ea099443f19b118119cb607f0830
Haneen <haneen.frontend@euruqalsahra.com>
2025-01-07 00:12:32 +0300

    test weekly-change

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const getCryptoPrices = require('./utils/cryptoPrices');
+const weeklyChangeRoutes = require('./routes/weeklyChange');
 const app = express();
 require('dotenv').config();
 app.use(express.static('public'));
@@ -17,6 +18,8 @@ app.use(cors(corsOptions));
 
 
 app.use(bodyParser.json());
+
+app.use('/api', weeklyChangeRoutes);
 
 // Connect to MongoDB using the correct URI
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
